7335076dcc95fe8f39f4413bc4749d807be9bc28
Hanh Do <dovanhanh121192@gmail.com>
2022-02-13 23:04:11 +0700

    Remove unused code

--- FindingFalcone/FindingFalcone/Shared/Models/CellDisplayable.swift
+++ FindingFalcone/FindingFalcone/Shared/Models/CellDisplayable.swift
@@ -3,15 +3,6 @@ import UIKit
 typealias Action<T> = (T) -> Void
 typealias ButtonAction = () -> Void
 
-
 protocol CellDisplayable {
-    var action: ButtonAction? { get }
-    
     func extracCell(from tableView: UITableView, for indexPath: IndexPath) -> UITableViewCell
-}
-
-extension CellDisplayable {
-    var action: ButtonAction? {
-        return nil
-    }
 }
